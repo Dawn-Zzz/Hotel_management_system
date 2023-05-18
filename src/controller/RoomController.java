b5ac5f14fc12f92e1cea741aeea31a735288122d
PhatDongNai <phatnguyen5523@gmail.com>
2023-05-18 15:10:38 +0700

    search room

--- src/controller/RoomController.java
+++ src/controller/RoomController.java
@@ -2,9 +2,17 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.RowFilter;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 import view.BookRoomView;
 import view.RoomInfor;
@@ -58,6 +66,53 @@ public class RoomController implements ActionListener {
 			    }
 			}
 		}
+		SearchTable(roomView.getRoomTable(), roomView.getSearchBox());
+	}
+	
+	private void SearchTable(JTable table, JTextField textField) {
+		String rentalType = "";
+		if (roomView.getRentalTypeList().getSelectedItem() != null) {
+			if (roomView.getRentalTypeList().getSelectedItem() == "All")
+				rentalType = "";
+			else
+				rentalType = (String) roomView.getRentalTypeList().getSelectedItem();
+		}
+		
+		String currentStatus = "";
+		if (roomView.getStatusList().getSelectedItem() != null) {
+			if (roomView.getStatusList().getSelectedItem() == "All")
+				currentStatus = "";
+			else
+				currentStatus = (String) roomView.getStatusList().getSelectedItem();
+		}
+		
+		TableRowSorter<TableModel> sorter = new TableRowSorter<>(table.getModel());
+		table.setRowSorter(sorter);
+		List<RowFilter<Object,Object>> filters = new ArrayList<>();
+
+        RowFilter<Object,Object> filter1 = RowFilter.regexFilter("(?i)" + rentalType,2);
+		RowFilter<Object,Object> filter2 = RowFilter.regexFilter("(?i)" + currentStatus,6);
+		filters.add(filter1);
+		filters.add(filter2);
+		sorter.setRowFilter(RowFilter.andFilter(filters));
+		textField.setText("");
+		
+		textField.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e) {
+				String input = textField.getText().trim();
+//				if (input.length() == 0) {
+//					sorter1.setRowFilter(null);
+//				} else {
+//					sorter1.setRowFilter(RowFilter.regexFilter("(?i)" + input));
+//				}
+				filters.clear();
+	            RowFilter<Object,Object> filter3 = RowFilter.regexFilter("(?i)" + input, 0, 1,3,4);
+	            filters.add(filter1);
+	            filters.add(filter2);
+	            filters.add(filter3);
+	            sorter.setRowFilter(RowFilter.andFilter(filters));
+			}
+		});
 	}
 
 }
